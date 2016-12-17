34499dc6aede492e2eeb2fb8d570b3a8d53c772f
191873 <191873@CARBON.CTI.P.LODZ.PL>
2016-12-17 16:04:45 +0100

    Dodanie podstaw do stworzenia testow

---
+++ src/test/java/bsearch/SequenceTest.java
@@ -0,0 +1,27 @@
+package bsearch;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.mockito.Mockito.mock;
+
+import edu.iis.mto.bsearch.SearchResult;
+
+
+public class SequenceTest {
+	
+	SearchResult searchResult;
+	
+	@Before
+	public void create()
+	{
+		searchResult = mock(SearchResult.class);
+		
+	}
+	
+	@Test
+	public void isInSequenceLengthOneTest()
+	{
+		
+	}
+
+}
