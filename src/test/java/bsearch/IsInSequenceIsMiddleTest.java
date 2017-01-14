f79c46652609b23f586770b193d47657e7e30f40
DawidKic <d.kicinski12@gmail.com>
2017-01-14 15:02:09 +0100

    Refaktoryzacja testu IsInSequenceIsMiddleTest

--- src/test/java/bsearch/IsInSequenceIsMiddleTest.java
+++ src/test/java/bsearch/IsInSequenceIsMiddleTest.java
@@ -1,6 +1,7 @@
 package bsearch;
 
-import static org.junit.Assert.assertSame;
+import org.junit.Assert;
+import static org.hamcrest.CoreMatchers.*;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -22,12 +23,12 @@ public class IsInSequenceIsMiddleTest {
 
 	@Test
 	public void isFoundTest() {
-		assertSame(searchResult.isFound(), true);
+		Assert.assertThat(searchResult.isFound(), is(true));
 	}
 
 	@Test
 	public void getPositionTest() {
-		assertSame(searchResult.getPosition(), position);
+		Assert.assertThat(searchResult.getPosition(), is(position));
 	}
 
 
